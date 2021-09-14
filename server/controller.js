508a87c51bf83fb4885962b4e0f5705d04e82ea1
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-14 14:58:13 -0600

    Finished with Update

--- server/controller.js
+++ server/controller.js
@@ -19,7 +19,23 @@ module.exports = {
             res.status(200).send(house)
     },
     updateHouse: (req, res) => {
-
+        const {id} = req.params
+        const {type} = req.body
+        let index = house.findIndex(h => h.id === +id)
+        
+        if (house[index].price === 0 && type === 'minus') {
+            res.status(400).send("You can't do that!")
+        }
+         else if (type === 'plus') {
+            house[index].price+=10000 
+            res.status(200).send(house)
+        } else if
+            (type === 'minus') {
+            house[index].price-=10000
+            res.status(200).send(house)
+        } if (house[index].price === 0 && type === 'minus') {
+            res.status(400).send("You can't do that!")
+        }
     },
     deleteHouse: (req, res) =>{
         const {id} = req.params
